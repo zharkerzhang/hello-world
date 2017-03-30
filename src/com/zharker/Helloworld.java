39774649d699306cf318f0f5adf6918a45190b0c
zharkerzhang <zharker@outlook.com>
2017-03-30 21:33:49 +0800

    share project test on idea

---
+++ src/com/zharker/Helloworld.java
@@ -0,0 +1,10 @@
+package com.zharker;
+
+/**
+ * Created by zharker on 2017/03/29.
+ */
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println(2333333);
+    }
+}
